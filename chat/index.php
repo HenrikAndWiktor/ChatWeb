8280b45c826a7a1168bba87ff537c1deb89fceda
HenrikAndWiktor <wiktor.istok.eriksson@gmail.com>
2017-09-20 17:14:02 +0200

    Official release

--- chat/index.php
+++ chat/index.php
@@ -5,22 +5,27 @@
 <table width="100%" border="1">
     <thead><tr><td><b>Meddelande</b></td><td><b>Namn</b></td></tr></thead>
     <?php
+    function unixtime($days = 30) {
+        return time()+60*60*24*$days;
+    }
     $conn = new mysqli("127.0.0.1","root",null,"CHATDB");
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
     if(isset($_POST["msg"])&&isset($_POST["name"])) {
+        setcookie("username",$_POST["name"],unixtime());
         $conn->query("INSERT INTO chattable (username,msg) VALUES (" . $_POST["name"] . "," . $_POST["msg"] . ");");
     }
     $mysql_table_res=$conn->query("SELECT * FROM chattable;");
-    while($row=$mysql_table_res->fetch_assoc()) {
+    while(($row=$mysql_table_res->fetch_assoc())!==null) {
         echo "<tr><td>" . $row["msg"] . "</td><td>" . $row["username"] . "</td></tr>";
     }
     ?>
 </table>
+<br><br><br><br><br><br><br><br><br><br><br><br><br>
 <form action="http://wiktoreriksson.se/ChatWeb/chat/index.php" method="POST">
-    <input type="text" name="name" value="">
-    <textarea name="msg" cols="200" rows="10"></textarea>
+    Namn:<input type="text" name="name" value='<?php echo (isset($_COOKIE["username"]))?$_COOKIE["username"]:"";?>'>
+    <label for="text-label">Medd:</label><textarea name="msg" id="text-label" cols="200" rows="10"></textarea>
     <input type="submit">
 </form>
 </body>
